c14b6052576102b3d7f12aab7d2ac20ed087c90a
Ray Britton <ray.britton@mubaloo.com>
2020-03-22 08:28:01 +0000

    Add methods for +/- month for NaiveDateTime and is leap year and days in months methods

--- src/extensions.rs
+++ src/extensions.rs
@@ -7,6 +7,8 @@ pub trait Utils {
     fn plus_one_hour(&self) -> Self;
     fn minus_one_day(&self) -> Self;
     fn plus_one_day(&self) -> Self;
+    fn plus_one_month(&self) -> Self;
+    fn minus_one_month(&self) -> Self;
 }
 
 impl Utils for NaiveDateTime {
@@ -24,6 +26,39 @@ impl Utils for NaiveDateTime {
 
     fn plus_one_day(&self) -> Self {
         return self.add(Duration::days(1));
+    }
+
+    fn plus_one_month(&self) -> Self {
+        let days = days_in_month(self.month() as u8, self.year() as u32);
+        let start = self.with_day(1).expect("Day 1 invalid (plus one month)");
+        return start.add(Duration::days((days + 1) as i64));
+    }
+
+    fn minus_one_month(&self) -> Self {
+        return self.with_day(1).expect("Day 1 invalid (minus one month [a])")
+            .sub(Duration::days(1))
+            .with_day(1).expect("Day 1 invalid (minus one month [b])");
+    }
+}
+
+pub fn is_leap_year(year: u32) -> bool {
+    return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+}
+
+/// Returns days in a month (accounting for leap years)
+/// Month are 1 based (i.e Jan = 1, Dec = 12)
+pub fn days_in_month(month: u8, year: u32) -> u8 {
+    match month {
+        2 => {
+            if is_leap_year(year) {
+                29
+            } else {
+                28
+            }
+        }
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        _ => panic!("Invalid month: {}", month)
     }
 }
 
@@ -46,12 +81,44 @@ fn unit() {
     return ();
 }
 
-pub trait MapToUnit <E> {
+pub trait MapToUnit<E> {
     fn map_to_unit(self) -> Result<(), E>;
 }
 
-impl <U,E> MapToUnit<E> for Result<U, E> {
+impl<U, E> MapToUnit<E> for Result<U, E> {
     fn map_to_unit(self) -> Result<(), E> {
         return self.map(|_| unit());
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_days_in_month() {
+        assert_eq!(days_in_month(1, 1900), 31, "Jan has 31");
+        assert_eq!(days_in_month(2, 1900), 31, "Feb has 28 on non leap years");
+        assert_eq!(days_in_month(3, 1900), 31, "Mar has 31");
+        assert_eq!(days_in_month(4, 1900), 31, "Apr has 30");
+        assert_eq!(days_in_month(5, 1900), 31, "May has 31");
+        assert_eq!(days_in_month(6, 1900), 31, "Jun has 30");
+        assert_eq!(days_in_month(7, 1900), 31, "Jul has 31");
+        assert_eq!(days_in_month(8, 1900), 31, "Aug has 31");
+        assert_eq!(days_in_month(9, 1900), 31, "Sep has 30");
+        assert_eq!(days_in_month(10, 1900), 31, "Oct has 31");
+        assert_eq!(days_in_month(11, 1900), 31, "Nov has 30");
+        assert_eq!(days_in_month(12, 1900), 31, "Dec has 31");
+        assert_eq!(days_in_month(1, 2000), 31, "Jan always has 31");
+        assert_eq!(days_in_month(2, 2000), 31, "Feb has 29 on leap years");
+    }
+
+    #[test]
+    fn test_leap_year() {
+        assert!(!is_leap_year(1900), "is not leap year");
+        assert!(!is_leap_year(2100), "is not leap year");
+        assert!(!is_leap_year(2101), "is not leap year");
+        assert!(is_leap_year(2000), "is leap year");
+        assert!(is_leap_year(2016), "is leap year");
     }
 }
